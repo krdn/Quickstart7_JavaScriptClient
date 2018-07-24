27912ace755834b479656806fc15c8d87995926c
김석곤 <krdn.net@gmail.com>
2018-07-24 11:55:16 +0900

    angular-auth-oidc-client 프로젝트 추가

---
+++ AngularClient-angular-oauth2-oidc/ClientApp/src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+// 아래 추가됨.
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { ConfigurationService } from "./configuration/configuration.service";
+// 아래 추가
+import { AuthModule, OidcSecurityService } from 'angular-auth-oidc-client';
+import { AuthService } from './services/auth.service';
+
+import { CallApiComponent } from './call-api/call-api.component';
+
+// 아래 추가
+const appInitializerFn = (appConfig: ConfigurationService) => {
+  return () => {
+    return appConfig.loadConfig();
+  };
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    CallApiComponent,
+    UnauthorizedComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    AuthModule.forRoot(), // 추가
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'call-api', component: CallApiComponent }, //,  canActivate: [AuthGuard] },
+      { path: 'unauthorized', component: UnauthorizedComponent } // 추가
+    ])
+  ],
+  providers: [ // 모두 추가됨.
+    { provide: 'ORIGIN_URL', useFactory: getBaseUrl }, 
+    AuthService,
+
+    OidcSecurityService,
+    ConfigurationService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFn,
+      multi: true,
+      deps: [ConfigurationService]
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+// 아래 추가
+export function getBaseUrl() {
+  return document.getElementsByTagName('base')[0].href;
+}
---
+++ AngularClient-angular-oauth2-oidc/ClientApp/src/app/call-api/call-api.component.ts
@@ -0,0 +1,53 @@
+//import { Component, OnInit } from '@angular/core';
+//import { AuthService } from '../services/auth.service';
+//import { ConfigurationService } from "../configuration/configuration.service";
+
+//@Component({
+//  selector: 'app-call-api',
+//  templateUrl: './call-api.component.html',
+//  styleUrls: ['./call-api.component.css']
+//})
+//export class CallApiComponent implements OnInit {
+//  public temp: JSON;
+//  constructor(authService: AuthService, configuration: ConfigurationService) {
+
+//    authService.get('http://localhost:5001/identity')
+//     //authService.get(configuration.apiAddress + 'identity')
+//      .subscribe(result => {
+//        this.temp = result;
+//        //this.forecasts = result as WeatherForecast[];
+//      }, error => console.error(error));
+//  }
+
+//  ngOnInit() {
+//  }
+
+//}
+
+
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { ConfigurationService } from "../configuration/configuration.service";
+
+@Component({
+  selector: 'app-call-api',
+  templateUrl: './call-api.component.html',
+  styleUrls: ['./call-api.component.css']
+})
+export class CallApiComponent {
+  public forecasts: WeatherForecast[];
+
+  constructor(authService: AuthService, configuration: ConfigurationService) {
+    authService.get(configuration.apiAddress + 'SampleData/WeatherForecasts')
+      .subscribe(result => {
+        this.forecasts = result as WeatherForecast[];
+      }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  dateFormatted: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
