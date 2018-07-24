c1e04bda014a74a1db1b34f02aeafcb647815404
김석곤 <krdn.net@gmail.com>
2018-07-24 12:07:59 +0900

    angular-auth-oidc-client 관련코드 삭제함

--- AngularClient-angular-oauth2-oidc/ClientApp/src/app/call-api/call-api.component.ts
+++ AngularClient-angular-oauth2-oidc/ClientApp/src/app/call-api/call-api.component.ts
@@ -26,8 +26,7 @@
 
 
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { ConfigurationService } from "../configuration/configuration.service";
+
 
 @Component({
   selector: 'app-call-api',
@@ -37,11 +36,11 @@ import { ConfigurationService } from "../configuration/configuration.service";
 export class CallApiComponent {
   public forecasts: WeatherForecast[];
 
-  constructor(authService: AuthService, configuration: ConfigurationService) {
-    authService.get(configuration.apiAddress + 'SampleData/WeatherForecasts')
-      .subscribe(result => {
-        this.forecasts = result as WeatherForecast[];
-      }, error => console.error(error));
+  constructor() {
+    //authService.get(configuration.apiAddress + 'SampleData/WeatherForecasts')
+    //  .subscribe(result => {
+    //    this.forecasts = result as WeatherForecast[];
+    //  }, error => console.error(error));
   }
 }
 
